7f48eff01e8a047b56e1d920115cb317823d47e8
bertrandmd <bertrand.md@gmail.com>
2020-02-21 14:58:20 +0100

    Initial Commit

---
+++ livemap-ios-sdk-sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  livemap-ios-sdk-sample
+//
+//  Created by Bertrand Mathieu-Daudé on 21/02/2020.
+//  Copyright © 2020 Bertrand Mathieu-Daudé. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
